edd95f73cb053a30b62ccc65f40da56f2b86eb21
David Speare <speare.david@gmail.com>
2016-04-03 11:39:04 -0400

    math_3d is now marked as changed by me
    I just read through the licence for my math library (and this program as
    a whole) and it turns out I'm supposed to note when I change someone
    else's file so the original author doesn't get blamed for my
    incompetence.

--- glTest2/glTest2/math_3d.h
+++ glTest2/glTest2/math_3d.h
@@ -15,6 +15,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+/*
+Changed by David Speare 
+*/
 
 #ifndef MATH_3D_H
 #define	MATH_3D_H
